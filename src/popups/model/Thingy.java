06fc62edcdf32c43553adf5caf16330a80c55752
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-27 07:57:55 -0600

    added repo and file

---
+++ src/popups/model/Thingy.java
@@ -0,0 +1,55 @@
+package popups.model;
+
+public class Thingy
+{
+
+	private int number;
+	private double value;
+	private String words;
+	private boolean isValid;
+	
+
+	//no constructor
+	
+	
+
+	//getters and setters here
+	
+	public int getnumber()
+	{
+		return number;
+	}
+	public double getvalue()
+	{
+		return value;
+	}
+	public String getwords()
+	{
+		return words;
+	}
+	public boolean getisValid()
+	{
+		return isValid;
+	
+	
+	}
+
+	
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+	public void setvalue(double value)
+	{
+		this.value = value;
+	}
+	public void setwords(String words)
+	{
+		this.words = words;
+	}
+	public void setisValid(boolean isValid)
+	{
+		this.isValid = isValid;
+	}
+	
+}
