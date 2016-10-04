fcd8e7144057e4f11d261c60172a5ca0cc0d4e3f
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-04 09:09:11 -0600

    added a ton of list code

--- src/popup/controller/PopupController.java
+++ src/popup/controller/PopupController.java
@@ -79,6 +79,45 @@ public class PopupController
 		Thingy testThingy = new Thingy();
 		thingyList.add(testThingy);
 		display.displayMessage(thingyList.size() + " is the size of the list.");
+		
+		for (int index = 0; index <5; index ++)
+		{
+			Thingy addThingy = new Thingy();
+			thingyList.add(addThingy);
+			display.displayMessage(addThingy.getwords());
+	
+		}
+	
+		display.displayMessage(thingyList.size() + " is the size.");
+	
+		
+		thingyList.get(3).setwords("look I changed a value");
+		
+		Thingy temp = thingyList.get(4);
+		temp.setwords("stuff");	
+		
+		for ( int index =0; index <thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index).getwords());
+		}
+		
+		thingyList.add(3, new Thingy());
+		
+		display.displayMessage("The size is: " + thingyList.size());
+		
+		thingyList.remove(0); // i no longer have access to this object because there is no = sign
+		
+		display.displayMessage("The size is " + thingyList.size());
+		
+		Thingy deletedThingy = thingyList.remove(0);
+		display.displayMessage(deletedThingy.getwords() + "and the list size is " + thingyList.size());
+		
+		thingyList.set(0, deletedThingy);
+		
+		for(int index = 0; index < thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index).getwords());
+		}
 	}
 }
 
